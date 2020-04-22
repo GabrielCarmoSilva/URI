fb6b964533c71d40bb6df592a6676766015e7a61
Gabriel Silva <35307664+GabrielCarmoSilva@users.noreply.github.com>
2020-04-22 20:34:13 -0300

    Create 2373.cpp

---
+++ 2373.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N, L, C, soma=0;
+	cin >> N;
+	for (int i=0; i < N; i++) {
+		cin >> L >> C;
+		if (L > C) {
+			soma += C;
+		}
+	}
+	cout << soma << endl;
+	return 0;
+}
